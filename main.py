fa0cb6f3a634cc48c58ee1e0c1f3da7be86f0536
killthebee <superhotfire447@yandex.ru>
2021-05-29 22:26:13 +0300

    Revert "make async archiving and writing to .zip"
    This reverts commit 57b67211

--- main.py
+++ main.py
@@ -1,35 +1,4 @@
 import os
-import subprocess
-import asyncio
-import aiofiles
-
-# subprocess.call("gedit")
-# program = "gedit"
-# process = subprocess.Popen(program)
-# code = process.wait()
-
-# print(code)
-
-# process = subprocess.Popen(['zip', '-r', '-', 'test'], stdout=subprocess.PIPE)
-# archive, _ = process.communicate()
-
-async def write_to_file(data):
-    async with aiofiles.open('test.zip', 'wb') as f:
-        await f.write(data)
+import zipfile
 
 
-async def archive(path):
-    proc = await asyncio.create_subprocess_shell(f'zip -r - {path}', stdout=asyncio.subprocess.PIPE)
-    archive = b''
-    while True:
-        chunk = await proc.stdout.read(100)
-        if proc.stdout.at_eof():
-            break
-        archive += chunk
-    print(archive)
-    await write_to_file(archive)
-    # with open('test.zip', 'wb') as outfile:
-    #     outfile.write(archive)
-
-
-asyncio.run(archive('test1'))
