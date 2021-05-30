b9bcf65ae05584256bf2f3551997f17e1f360397
killthebee <superhotfire447@yandex.ru>
2021-05-30 10:28:00 +0300

    somewhat server

---
+++ server.py
@@ -0,0 +1,37 @@
+import aiofiles
+import datetime
+import asyncio
+
+from aiohttp import web
+
+INTERVAL_SECS = 1
+
+
+async def archivate(request):
+    response = web.StreamResponse()
+    response.headers['Content-Type'] = 'text/html'
+
+    await response.prepare(request)
+
+    while True:
+        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f'{formatted_date}<br>'
+        await response.write(message.encode('utf-8'))
+        await asyncio.sleep(INTERVAL_SECS)
+
+
+async def handle_index_page(request):
+    async with aiofiles.open('index.html', mode='r') as index_file:
+        index_contents = await index_file.read()
+    return web.Response(text=index_contents, content_type='text/html')
+
+
+
+
+if __name__ == '__main__':
+    app = web.Application()
+    app.add_routes([
+        web.get('/', handle_index_page),
+        web.get('/archive/{archive_hash}/', archivate),
+    ])
+    web.run_app(app)
